58ae40b154aaa3a0590dbfc69802331c2af15a3d
eunkyoung <eunkyoung4160@gmail.com>
2025-01-29 12:54:29 +0900

    feat: LLM 함수 기능 구현

--- closet/views.py
+++ closet/views.py
@@ -14,3 +14,62 @@ def closet_start_view(request):
 @login_required
 def closet_history_view(request):
     return render(request, 'closet_history.html')
+
+
+
+
+from google import genai
+from google.genai import types
+import base64
+
+def generate():
+  client = genai.Client(
+      vertexai=True,
+      project="sonic-progress-449301-b7",
+      location="us-central1"
+  )
+
+
+  model = "gemini-2.0-flash-exp"
+  contents = [
+    types.Content(
+      role="user",
+      parts=[
+        types.Part.from_text("""기온이 20도정도 되는 날씨의 옷""")
+      ]
+    ),
+   ]
+  tools = [
+    types.Tool(google_search=types.GoogleSearch())
+  ]
+  generate_content_config = types.GenerateContentConfig(
+    temperature = 1,
+    top_p = 0.95,
+    max_output_tokens = 8192,
+    response_modalities = ["TEXT"],
+    safety_settings = [types.SafetySetting(
+      category="HARM_CATEGORY_HATE_SPEECH",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_DANGEROUS_CONTENT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_HARASSMENT",
+      threshold="OFF"
+    )],
+    tools = tools,
+  )
+
+  for chunk in client.models.generate_content_stream(
+    model = model,
+    contents = contents,
+    config = generate_content_config,
+    ):
+    if not chunk.candidates or not chunk.candidates[0].content.parts:
+        continue
+    print(chunk.text, end="")
+
+generate()
