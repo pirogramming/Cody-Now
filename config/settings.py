eecc48052ed336201e2f64e79f2f24a54704b02d
junhkchoi <leisure1566@gmail.com>
2025-01-29 11:49:15 +0900

    Refactor: settings.py 보안 코드 .env로 이전

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -185,9 +186,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'closet:dashboard'
 
 # 이메일 설정 
+load_dotenv()  # .env 파일 로드
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'codynoww@gmail.com'
-EMAIL_HOST_PASSWORD = 'ggxwqwpzhkdemzme'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
