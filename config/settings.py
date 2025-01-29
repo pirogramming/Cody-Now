8704b1c28fe64e3e0417dffe767ea35d7130b936
junhkchoi <leisure1566@gmail.com>
2025-01-29 11:30:23 +0900

    Feat: PW 이메일 인증 기능 구현

--- config/settings.py
+++ config/settings.py
@@ -184,5 +184,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'closet:dashboard'
 
-# 이메일 설정 (개발용)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# 이메일 설정 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'codynoww@gmail.com'
+EMAIL_HOST_PASSWORD = 'ggxwqwpzhkdemzme'
--- user/urls.py
+++ user/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('auth/', include('social_django.urls', namespace='social')),
-
 ]
