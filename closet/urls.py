5785bcedd95ed4afe4c68943acb596a4ff04f4f3
junhkchoi <leisure1566@gmail.com>
2025-02-01 16:55:12 +0900

    Feat: 프롬포트 엔지니어링, 옷장 평가 페이지

--- closet/urls.py
+++ closet/urls.py
@@ -1,6 +1,8 @@
 #urls.py
 from django.urls import path
 from closet import views
+from .views import evaluate_closet
+
 
 app_name = 'closet'
 
@@ -11,6 +13,9 @@ urlpatterns = [
     path('upload/', views.upload_outfit, name='upload_outfit'),
     path('gen-cody/', views.gen_cody, name='gen_cody'),
     path('post-analysis/', views.post_analysis, name='post_analysis'),  # 분석 결과 저장용 URL
+    # path('evaluate_closet/', evaluate_closet, name='evaluate_closet'),
+
+    path('evaluate_closet/', evaluate_closet, name='evaluate_closet'),
 ]
 
 
