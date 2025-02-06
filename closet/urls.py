6225b5984e02913018e49b95ced2c7ed3ef71826
junhkchoi <leisure1566@gmail.com>
2025-02-06 17:06:06 +0900

    fix: 체험하기 업로드 페이지 수정

--- closet/urls.py
+++ closet/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from closet import views
 from .views import evaluate_closet
+from .views import test_image_upload_html
 
 
 app_name = 'closet'
@@ -26,7 +27,7 @@ urlpatterns = [
     path('bookmark/<int:outfit_id>/', views.toggle_bookmark, name='toggle_bookmark'),
     path('delete/<int:outfit_id>/', views.delete_outfit, name='delete_outfit'),
     path('api/outfit/<int:outfit_id>/', views.get_outfit_data, name='get_outfit_data'), #페이지 연결
-    path("test-image-upload/", views.test_image_upload, name="test_image_upload"), #테스트용에서 이미지 업로드
+    path('test-image-upload/', test_image_upload_html, name='test_image_upload'),
     path("test-input/", views.test_input_page, name="test_input_page"), #테스트용 이미지 업로드 할 수 있는 페이지
     
 ]
