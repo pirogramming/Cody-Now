0cc97142b887d8dfcf86bcc0c633a420a2744fc5
junhkchoi <leisure1566@gmail.com>
2025-02-10 15:30:23 +0900

    Remove: 기존 체험하기 관련 html

--- closet/urls.py
+++ closet/urls.py
@@ -28,8 +28,6 @@ urlpatterns = [
     path('bookmark/<int:outfit_id>/', views.toggle_bookmark, name='toggle_bookmark'),
     path('delete/<int:outfit_id>/', views.delete_outfit, name='delete_outfit'),
     path('api/outfit/<int:outfit_id>/', views.get_outfit_data, name='get_outfit_data'), #페이지 연결
-    # path('test-image-upload/', test_image_upload_html, name='test_image_upload'),
-    # path("test-input/", views.test_input_page, name="test_input_page"), #테스트용 이미지 업로드 할 수 있는 페이지
     path('upload-history/', views.upload_history, name="upload_history"),
 
 
